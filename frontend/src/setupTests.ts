a40996ddb815582f7e80c35aaf48d82817fa21ac
Chris Poupart <chris.poupart@gmail.com>
2025-07-14 16:08:06 -0400

    test(setupTests): 🤡 enforce mocking of all API requests in tests

--- frontend/src/setupTests.ts
+++ frontend/src/setupTests.ts
@@ -1,1 +1,30 @@
-import '@testing-library/jest-dom'; 
+import '@testing-library/jest-dom';
+
+// Fail tests if any unmocked fetch is called
+beforeAll(() => {
+  const originalFetch = global.fetch;
+  global.fetch = (...args) => {
+    throw new Error(
+      `Unexpected fetch call in test: ${args[0]}. You must mock all API requests.`
+    );
+  };
+  // Optionally, restore fetch after all tests
+  afterAll(() => {
+    global.fetch = originalFetch;
+  });
+});
+
+// Fail tests if any unmocked axios request is made
+import axios from 'axios';
+beforeAll(() => {
+  const originalAxios = axios.request;
+  axios.request = function (...args) {
+    throw new Error(
+      `Unexpected axios request in test: ${JSON.stringify(args[0])}. You must mock all API requests.`
+    );
+  };
+  // Optionally, restore axios after all tests
+  afterAll(() => {
+    axios.request = originalAxios;
+  });
+}); 
